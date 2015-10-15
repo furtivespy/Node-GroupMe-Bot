2507da588e4fedfcb8e4c86ebc9ef7729854cca8
Will <furtivespy@gmail.com>
2015-10-14 21:19:50 -0400

    make json
    json good times

--- yesnoBot.js
+++ yesnoBot.js
@@ -3,10 +3,10 @@ var request = require('request');
 exports.respond = function(theRequest, callback){
   if (theRequest.text && theRequest.text.trim().toLowerCase().startsWith('should i ')){
     console.log('YES or NO?');
-    request('http://yesno.wtf/api', function(error, response, body) {
-      console.log(JSON.stringify(body));
-      callback(true, body);
-      //callback(true, body.image);
+    request({url: 'http://yesno.wtf/api', json: true }, function(error, response, body) {
+      //console.log(JSON.stringify(body));
+      callback(true, body.answer);
+      callback(true, body.image);
     });
   } else {
     callback(false);
