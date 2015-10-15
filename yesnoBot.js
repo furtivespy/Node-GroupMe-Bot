778873fe632eaf28170ef1e9f5777c971c46bbf5
Will <furtivespy@gmail.com>
2015-10-14 21:14:27 -0400

    bot
    fix

--- yesnoBot.js
+++ yesnoBot.js
@@ -4,8 +4,9 @@ exports.respond = function(theRequest, callback){
   if (theRequest.text && theRequest.text.trim().toLowerCase().startsWith('should i ')){
     console.log('YES or NO?');
     request('http://yesno.wtf/api', function(error, response, body) {
-      callback(true, body.answer);
-      callback(true, body.image);
+      console.log(JSON.stringify(body));
+      callback(true, body);
+      //callback(true, body.image);
     });
   } else {
     callback(false);
