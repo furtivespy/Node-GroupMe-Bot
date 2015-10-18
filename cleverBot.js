be9398babcdcd8f5a7943577fd973a2161d5b153
Will <furtivespy@gmail.com>
2015-10-17 20:28:11 -0400

    add cleverbot
    bots

---
+++ cleverBot.js
@@ -0,0 +1,29 @@
+var Cleverbot = require('Cleverbot.io');
+
+exports.respond = function(theRequest, callback){
+  if (theRequest.text && (
+        theRequest.text.trim().toLowerCase().startsWith('robot, ') ||
+        theRequest.text.trim().toLowerCase().startsWith('robot: ')
+        theRequest.text.trim().toLowerCase().startsWith('robot friend, ')
+        theRequest.text.trim().toLowerCase().startsWith('robot friend: ')
+      ){
+    console.log('cleverbot');
+    var trim;
+    if (theRequest.text.trim().toLowerCase().startsWith('robot friend'){
+      trim = 14; //remove "robot friend, "
+    } else {
+      trim = 7; //remove "robot, "
+    }
+    var bot = new Cleverbot(process.env.CLEVERBOT_USER, process.env.CLEVERBOT_KEY); // Set up keys
+
+    bot.setNick(process.env.CLEVERBOT_SESSION); // Set a nickname
+
+    bot.create(function (err, session) { // Initialize Cleverbot
+    	bot.ask(theRequest.text.trim().substring(trim), function (err, response) {
+    		callback(true, response);
+    	});
+    });
+  } else {
+    callback(false);
+  }
+}
