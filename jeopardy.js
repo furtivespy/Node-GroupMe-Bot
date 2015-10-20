89540129b2bc9bcd4c7551883c3019bc8ed6f89b
Will <furtivespy@gmail.com>
2015-10-20 00:43:36 -0400

    that was stupid
    fixed

--- jeopardy.js
+++ jeopardy.js
@@ -13,7 +13,7 @@ exports.respond = function(theRequest, callback){
   client.get('isQuestionOutstanding', function(err, isQuestionOutstanding) {
     if (theRequest.text && theRequest.text.trim().toLowerCase().startsWith('jeopardy')){
       console.log('jeopardy');
-      if (isQuestionOutstanding == 'true'){
+      if (isQuestionOutstanding == 'yes'){
         client.get('currentAnswer', function(er, currentAnswer) {
           callback(true, "The previous answer was: " + currentAnswer);
           sendNewQuestion(callback);
@@ -21,12 +21,12 @@ exports.respond = function(theRequest, callback){
       } else {
         sendNewQuestion(callback);
       }
-    } else if (theRequest.text && isQuestionOutstanding && (theRequest.text.trim().toLowerCase().startsWith('a:') || theRequest.text.trim().toLowerCase().startsWith('a-'))){
+    } else if (theRequest.text && isQuestionOutstanding == 'yes' && (theRequest.text.trim().toLowerCase().startsWith('a:') || theRequest.text.trim().toLowerCase().startsWith('a-'))){
       client.get('currentAnswer', function(er, currentAnswer) {
         console.log('Answer is ');
         if(currentAnswer.toLowerCase() == theRequest.text.toLowerCase().substring(2).trim()){
           console.log('Answer is correct!');
-          client.set('isQuestionOutstanding', 'false');
+          client.set('isQuestionOutstanding', 'no');
           client.get('currentValue', function(anErr, currentValue) {
               client.incrby("score:" + theRequest.sender_id, currentValue, function (anotherErr, newScore) {
                 callback(true, "Correct! " + theRequest.name + ", you now have $" + newScore);
