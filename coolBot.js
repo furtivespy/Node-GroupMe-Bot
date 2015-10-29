ced01037392cc6f13416946c5a723c23d747d82b
Will <furtivespy@gmail.com>
2015-10-29 13:08:33 -0400

    clean up help
    help clean

--- coolBot.js
+++ coolBot.js
@@ -8,13 +8,15 @@ exports.respond = function(theRequest, callback){
     console.log('cool face');
     callback(true, cool())
   } else if (theRequest.text && (theRequest.text.trim().toLowerCase().startsWith('robot help') || theRequest.text.trim().toLowerCase().startsWith('robohelp'))){
-    callback(true, 'I\'m a robot and I respond to certain messages that start with:');
-    callback(true, '"/cool guy" and I make a funny face ' + cool());
-    callback(true, '"Should I" and I\'ll let you know if you should.');
-    callback(true, '"roll [dice]" and I\'ll roll some dice for you.');
-    callback(true, '"robot," or "robot friend," and you can chat with me.');
-    callback(true, '"Jeopardy" for some trivia. use "a-" to try and answer.');
-    callback(true, '"gm [search]" to search gifme for a gif or "g [search]" to search giphy and gifme for a gif.');
+    callback(true, 'I\'m a robot and I respond to certain messages that start with: \n' +
+                    '"/cool guy" and I make a funny face. \n' +
+                    '"Should I" and I\'ll let you know if you should.\n' +
+                    '"roll [dice]" and I\'ll roll some dice for you.\n' +
+                    '"robot," or "robot friend," and you can chat with me.\n' +
+                    '"Jeopardy" for some trivia. use "a-" to try and answer.\n' +
+                    '"gm [search]" to search gifme for a gif or "g [search]" to search giphy and gifme for a gif.\n' +
+                    'And of course you found this "robohelp" or "robot help" ' + cool());
+
   } else {
     console.log('no face');
     callback(false);
