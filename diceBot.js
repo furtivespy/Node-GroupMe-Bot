3010c50395d1f1b4e2632a30cf2dbbf30563ec80
Will <furtivespy@gmail.com>
2016-02-17 22:47:28 -0500

    dice update
    dice

--- diceBot.js
+++ diceBot.js
@@ -4,11 +4,11 @@ exports.respond = function(theRequest, callback){
   if (theRequest.text && theRequest.text.trim().toLowerCase().startsWith('/roll ')){
     console.log('Let\'s Roll');
     var roll = new Roll();
-    if (roll.validate(theRequest.text.trim().substring(5))){
-      var rolling = roll.roll(theRequest.text.trim().substring(5));
+    if (roll.validate(theRequest.text.trim().substring(6))){
+      var rolling = roll.roll(theRequest.text.trim().substring(6));
       callback(true, theRequest.name + ' rolled ' + rolling.rolled + ' for a total of: ' + rolling.result);
     } else {
-      callback(true, 'I don\'t know how to roll ' + theRequest.text.trim().substring(5));
+      callback(true, 'I don\'t know how to roll ' + theRequest.text.trim().substring(6));
     }
   } else {
     callback(false);
