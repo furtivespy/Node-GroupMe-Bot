11f20d56856df705c206144918eeaeb3c2fd612a
Will <furtivespy@gmail.com>
2016-02-19 18:37:55 -0500

    change to reply to any should question
    more yes/nos

--- yesnoBot.js
+++ yesnoBot.js
@@ -1,7 +1,7 @@
 var request = require('request');
 
 exports.respond = function(theRequest, callback){
-  if (theRequest.text && theRequest.text.trim().toLowerCase().startsWith('should i ')){
+  if (theRequest.text && theRequest.text.trim().toLowerCase().startsWith('should ')){
     console.log('YES or NO?');
     request({url: 'http://yesno.wtf/api', json: true }, function(error, response, body) {
       //console.log(JSON.stringify(body));
