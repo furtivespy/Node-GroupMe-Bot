37f1ce5ae3d47ceb441d7584758261516ada025d
Will <furtivespy@gmail.com>
2016-02-23 17:28:16 -0500

    rules for new fun
    added

--- coolBot.js
+++ coolBot.js
@@ -29,6 +29,7 @@ exports.respond = function(theRequest, callback){
                     '"gm [search]" to search gifme for a gif or "g [search]" to search giphy and gifme for a gif.\n' +
                     '"/cool guy" is a fun face. "/joke" is a fun joke.\n' +
                     '"/advice" for a small piece of advice. "/cat" for a cat fact. \n' +
+                    '"/simpsons" for a Simpsons quote. "/insult {someone}" and I\'ll direct an insult at {someone}. \n' +
                     '"/slogan {something}" and I\'ll make up a slogan for that something. \n' +
                     '"/chuck", "/chuck me", or "/chuck {insert name here}" for a Chuck Norris type joke.\n' +
                     '"/flip {something}" and I\'ll flip that something, but "/unflip" and I\'ll put it back.\n' +
