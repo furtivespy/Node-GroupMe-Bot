76053dd4ab2647aa8c0d5f4cd0f7b2391df301b4
Will <furtivespy@gmail.com>
2016-02-27 13:44:35 -0500

    testing info
    added

--- webscrapeBot.js
+++ webscrapeBot.js
@@ -54,7 +54,9 @@ function getFriends(theRequest, callback){
     	$ = cheerio.load(body);
     	var ranItem = Math.floor( (Math.random() * $('dl').length) + 1 );
     	var quote = '';
+    	console.log('dl:nth-child(' + ranItem + ')');
     	$('dl:nth-child(' + ranItem + ')').children('dd').each(function(i,elem){
+    		console.log($(this).text().replace(/<.*?>/g,''));
     		quote = quote +  $(this).text().replace(/<.*?>/g,'') + '\n';
     	});
     	callback(true, quote);
